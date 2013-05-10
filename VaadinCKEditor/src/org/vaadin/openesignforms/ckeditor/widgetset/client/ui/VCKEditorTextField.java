9dbc67130247919e7c57315634a2f0f606a9858d
yozons <yozons@750c7896-20b4-11df-9eea-e10578fb8014>
2013-05-10 19:15:35 +0000

    modal window fix

--- VaadinCKEditor/src/org/vaadin/openesignforms/ckeditor/widgetset/client/ui/VCKEditorTextField.java
+++ VaadinCKEditor/src/org/vaadin/openesignforms/ckeditor/widgetset/client/ui/VCKEditorTextField.java
@@ -19,10 +19,10 @@ import com.google.gwt.dom.client.Style.Overflow;
 import com.google.gwt.dom.client.Style.Visibility;
 import com.google.gwt.user.client.ui.Focusable;
 import com.google.gwt.user.client.ui.Widget;
-import com.vaadin.terminal.gwt.client.ApplicationConnection;
-import com.vaadin.terminal.gwt.client.EventId;
-import com.vaadin.terminal.gwt.client.Paintable;
-import com.vaadin.terminal.gwt.client.UIDL;
+import com.vaadin.client.ApplicationConnection;
+import com.vaadin.client.Paintable;
+import com.vaadin.client.UIDL;
+import com.vaadin.shared.EventId;
 
 /**
  * Client side CKEditor widget which communicates with the server. Messages from the
@@ -94,6 +94,7 @@ public class VCKEditorTextField extends Widget implements Paintable, CKEditorSer
 	/**
 	 * Called whenever an update is received from the server
 	 */
+	@Override
 	public void updateFromUIDL(UIDL uidl, ApplicationConnection client) {
 		clientToServer = client;
 		paintableId = uidl.getId();
